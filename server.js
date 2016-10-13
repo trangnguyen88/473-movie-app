8611b9d08ca198cdf6005f6c1a10a4a7ffbc1b78
Duy Do <ddo@csu.fullerton.edu>
2016-10-13 05:13:10 -0700

    Added endpoint to receive votes from client

--- server.js
+++ server.js
@@ -79,6 +79,37 @@ app.get('/movie', function(req, res) {
     })
 });
 
+app.post('/movie/title/vote', function(req, res) {
+
+    var vote = req.body.vote;
+    var name = req.body.title;
+    console.log('Stranger votes ' + vote + ' on ' + name);
+    Movie.findOne({ title: name }, function(err, result) {
+        if (err) {
+            res.json({ 'result': 'error' });
+            console.log("Error on accessing the database");
+            return;
+        }
+        var newVotes = result.meta.votes + 1;
+        var newLikes = result.meta.likes;
+        if (vote == 'yes') {
+            newLikes += 1;
+        }
+
+        Movie.update({ title: result.title }, { meta: { votes: newVotes, likes: newLikes } }, function(err, success) {
+
+            if (err) {
+                console.log("Error while updating database");
+                res.json({ 'result': 'error' });
+                return;
+            }
+            var reply = { 'result': 'success', 'newVotes': newVotes, 'newLikes': newLikes }
+            res.json(reply);
+        })
+    })
+
+});
+
 
 http.createServer(app).listen(3000);
 //set up our routes
